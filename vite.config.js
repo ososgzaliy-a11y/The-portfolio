98fd962467099039245c362fcda893218c251f12
Osama El-Ghazaliy <ososgzaliy@gmail.com>
2026-08-24 21:07:26 +0300

    Auto-update: live changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: 'remove-redirects-file',
+      closeBundle() {
+        const redirectsPath = path.resolve(__dirname, 'dist/_redirects');
+        if (fs.existsSync(redirectsPath)) {
+          fs.unlinkSync(redirectsPath);
+          console.log('Successfully purged dist/_redirects');
+        }
+      }
+    }
+  ]
+});
